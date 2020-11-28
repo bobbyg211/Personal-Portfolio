45074ea6a0183d7c6bf623e0650b82cb4bcad949
bobbyg211 <robertdg96@gmail.com>
2020-11-27 22:04:32 -0600

    Home Banner - Desktop Lg - Complete

--- js/general.js
+++ js/general.js
@@ -1,1 +1,17 @@
-console.log("general");
+// Main menu
+
+$(".header nav .hamburger").click(function () {
+  $(this).toggleClass("active");
+});
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 0) {
+    $(".header .logo a .name .inner").addClass("hide-name");
+  } else {
+    $(".header .logo a .name .inner").removeClass("hide-name");
+  }
+});
+
+setInterval(function () {
+  $(".header .sub-ribbon .dot-nav span.blue").toggleClass("active");
+}, 500);
